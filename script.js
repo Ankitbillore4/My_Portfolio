249dda99113d6bc31aee0fb55a6533d36ad5d7af
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-11-07 22:19:18 +0530

    js page added

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ module.exports = {
     },
   }
   
+//   const textWidth = document.querySelector('.SkillImages').scrollWidth;
+
+//  gsap.to(".SkillImages", {
+//    x: `-${textWidth}px`,
+//    duration: 40,
+//    repeat: -1,
+//    ease: "none",
+//    onRepeat: function() {
+//      gsap.set(this.targets(), { x: window.innerWidth });
+//    }
+//  });
+
+    
+
+// gsap.from(".SkillImages",{
+//   y: -100,
+//   duration:2,
+//   delay:2
+// })
